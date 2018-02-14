1a3235d89c33cbe71ff5d0a901799ba17e3b84f5
ysicing <root@ysicing.net>
2018-02-14 15:25:13 +0800

    update release

--- flask_qedit/finit.py
+++ flask_qedit/finit.py
@@ -10,15 +10,82 @@
 
 import os
 import click
+import subprocess
+
+
+class Project(object):
+
+    def __init__(self, pname, ppath, simple):
+        self.pname = pname
+        self.ppath = ppath
+        self.simple = simple
+
+    def repo(self):
+        if os.path.exists(self.ppath):
+            return False
+
+        os.mkdir(self.ppath)
+        return True
+
+    def pyenv(self, envpath, pythonv='python3'):
+        if os.path.exists(envpath):
+            return False
+        os.mkdir(envpath)
+        # virtualenv -p python3  ./test
+        try:
+            exec_cmd = subprocess.check_output(['virtualenv', '-p', pythonv, envpath])
+        except subprocess.CalledProcessError as e:
+            err_info = e.output
+            err_code = e.returncode
+            print('exec error:{}/{}'.format(err_info, err_code))
+            return False
+        print(exec_cmd.decode('utf-8'))
+        return True
+
+    def do_simple(self):
+        simple_py = '''
+#!/usr/bin/env python3
+# coding=utf-8
+
+
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def hello_world():
+    return 'Hello World!'
+
+
+if __name__ == '__main__':
+    app.run()
+
+        '''
+        with open(os.path.join(self.ppath, self.pname+'.py'), 'w') as f:
+            f.write(simple_py)
+        return True
+
+    def init(self):
+        if self.simple:
+            self.do_simple()
+        else:
+            # Todo
+            print("pass")
+        return True
 
 
 @click.group()
 def cli():
     """A simple cmd line tool"""
 
-
 @click.command(help='create new project')
-def new():
+@click.argument('project_name')
+@click.option('--path', default="~/ysbot/repo/")
+@click.option('--envpath', default="~/ysbot/pypi")
+@click.option('--simple', is_flag=True)
+def new(project_name, path, envpath):
+    project_path = os.path.join(path, project_name)
     click.echo("add record. {}".format(os.getcwd()))
 
 
--- release.sh
+++ release.sh
@@ -3,9 +3,17 @@
 DOCKER_IMG="cclouds/finit:pypi"
 DOCKER_CON="finit"
 
+function prepare(){
+
+    cmi=$(git log -n 1 --pretty --format=%h)
+    cp Dockerfile.pypi Dockerfile.release
+    sed -i "s#0.0.0#$cmi#g" Dockerfile.release
+
+}
+
 function run(){
     echo "docker build image"
-    docker build --no-cache -t $DOCKER_IMG -f Dockerfile.pypi .
+    docker build --no-cache -t $DOCKER_IMG -f Dockerfile.release .
     echo "release flask-init to pypi"
     docker run -it --rm  -v $(echo ~)/.pypirc:/root/.pypirc --name $DOCKER_CON $DOCKER_IMG release
     echo "docker delete image"
